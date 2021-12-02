c093fe5559061fc51a82cedcf1d643fe0c6fed97
Fabian Hofer <s2010237027@fhooe.at>
2021-12-02 20:23:01 +0100

    Initial Commit

---
+++ Target Support Files/Pods-Hofer_Pro-Hofer_ProUITests/Pods-Hofer_Pro-Hofer_ProUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Hofer_Pro_Hofer_ProUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Hofer_Pro_Hofer_ProUITests
+@end
---
+++ Target Support Files/Pods-Hofer_Pro/Pods-Hofer_Pro-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Hofer_Pro : NSObject
+@end
+@implementation PodsDummy_Pods_Hofer_Pro
+@end
